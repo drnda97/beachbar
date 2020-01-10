c620c334cd8225014b4022a972e48268191d061d
unknown <drndica54@gmail.com>
2020-01-10 16:47:40 +0100

    virtual host added

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="clearfix" id="nav">
   <div class="logo clearfix">
-      <a href="http://localhost/pebbles/public/"><img src="{{asset('images/logos/logo.png')}}" alt="logo" id="img_pos"></a>
+      <a href="/"><img src="{{asset('images/logos/logo.png')}}" alt="logo" id="img_pos"></a>
   </div>
   <div class="hamburger secret">
     <img src="{{asset('images/icons/1.png')}}" alt="hamburger-icon" id="hamburger_icon">
@@ -9,9 +9,9 @@
     <div class="close secret">
       <img src="{{asset('images/icons/2.png')}}" alt="x-icon" id="x_icon">
     </div>
-    <li><a href="http://localhost/pebbles/public/hoursandlocation">Hours & Location</a></li>
-    <li><a href="http://localhost/pebbles/public/menu">Menu</a></li>
-    <li><a href="http://localhost/pebbles/public/contact">Contact</a></li>
-    <li><a href="http://localhost/pebbles/public/feed">News Feed</a></li>
+    <li><a href="/hoursandlocation">Hours & Location</a></li>
+    <li><a href="/menu">Menu</a></li>
+    <li><a href="/contact">Contact</a></li>
+    <li><a href="/feed">News Feed</a></li>
   </ul>
 </nav>

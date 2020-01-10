4a1ae54ed4204c4e3e6fbd94c69595e6cc8e0c25
unknown <drndica54@gmail.com>
2020-01-10 16:43:49 +0100

    navigation done

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,13 +1,13 @@
 <nav class="clearfix" id="nav">
   <div class="logo clearfix">
-      <a href="http://localhost/pebbles/public/"><img src="{{asset('images/logos/logo.png')}}'" alt="logo" id="img_pos"></a>
+      <a href="http://localhost/pebbles/public/"><img src="{{asset('images/logos/logo.png')}}" alt="logo" id="img_pos"></a>
   </div>
   <div class="hamburger secret">
-    <img src="../public/assets/icons/1.png" alt="hamburger-icon" id="hamburger_icon">
+    <img src="{{asset('images/icons/1.png')}}" alt="hamburger-icon" id="hamburger_icon">
   </div>
   <ul class="nav_ul">
     <div class="close secret">
-      <img src="../public/assets/icons/2.png" alt="x-icon" id="x_icon">
+      <img src="{{asset('images/icons/2.png')}}" alt="x-icon" id="x_icon">
     </div>
     <li><a href="http://localhost/pebbles/public/hoursandlocation">Hours & Location</a></li>
     <li><a href="http://localhost/pebbles/public/menu">Menu</a></li>

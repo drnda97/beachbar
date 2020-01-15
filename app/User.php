209b850085d05ac8a62d7bc98ab8be88bcb003ae
Milan <drndica54@gmail.com>
2020-01-15 13:10:26 +0100

    database

--- app/User.php
+++ app/User.php
@@ -6,10 +6,8 @@ namespace App;
 // use Illuminate\Foundation\Auth\User as Authenticatable;
 // use Illuminate\Notifications\Notifiable;
 
-class User 
+class User extends Model
 {
    //It's for the user views
-   public static function select(){
-      // User::select('select * from images where id_option = ?', [5]);
-   }
+
 }
